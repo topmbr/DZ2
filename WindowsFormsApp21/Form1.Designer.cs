9edd337fed796911f6279e9b52483d5c0a7db61d
topmbr <centerm9911@gmail.com>
2023-10-08 12:07:54 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp21/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(493, 425);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(552, 70);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(178, 65);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Обзор";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(552, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp21/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Начальный путь (корневые диски)
+            string initialPath = @"C:\";
+
+            // Установка начального пути в TextBox
+            textBox1.Text = initialPath;
+
+            // Заполнение ListView файлами и папками из начального пути
+            LoadDirectory(initialPath);
+        }
+
+        private void LoadDirectory(string path)
+        {
+            // Очистка ListView
+            listView1.Items.Clear();
+
+            try
+            {
+                // Получение списка файлов и папок в указанной директории
+                string[] directories = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+
+                // Добавление папок
+                foreach (string directory in directories)
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(directory);
+                    ListViewItem item = new ListViewItem(dirInfo.Name);
+                    item.SubItems.Add("Папка");
+                    item.SubItems.Add(dirInfo.LastWriteTime.ToString());
+                    listView1.Items.Add(item);
+                }
+
+                // Добавление файлов
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    ListViewItem item = new ListViewItem(fileInfo.Name);
+                    item.SubItems.Add("Файл");
+                    item.SubItems.Add(fileInfo.LastWriteTime.ToString());
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при загрузке директории: " + ex.Message);
+            }
+        }
+
+        private void btnBrowse_Click_1(object sender, EventArgs e)
+        {
+            // Открыть диалоговое окно выбора папки
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Установка выбранного пути в TextBox
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+
+                // Загрузка файлов и папок из выбранной папки
+                LoadDirectory(folderBrowserDialog.SelectedPath);
+            }
+        }
+    }
+}
